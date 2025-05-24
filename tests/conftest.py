805c1d1e3748b4227628061596020ea58dde1c6b
natal <svetnata74@mail.ru>
2025-05-24 11:29:04 +0200

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,7 +28,7 @@ def setup_browser(request):
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
-    #driver_options.page_load_strategy = 'eager'
+    driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
     browser.config.window_width = 1920
     browser.config.window_height = 1080
@@ -54,6 +54,47 @@ def setup_browser(request):
         options=options)
 
     yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+    attach.add_logs(browser)
+
+    browser.quit()
+
+
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser.config.base_url = 'https://demoqa.com'
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    url = os.getenv('URL')
+
+    browser.config.driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@{url}/wd/hub",
+        options=options)
+
+    yield
+
 
     attach.add_screenshot(browser)
     attach.add_html(browser)
